06340ad19ee03fc1a3ccbb0ae3e0f65a115581ad
NhiNguyencmt8 <61450663+NhiNguyencmt8@users.noreply.github.com>
2023-08-10 14:30:04 -0400

    still testing

--- RealRobot/SimpleTorqueController.py
+++ RealRobot/SimpleTorqueController.py
@@ -7,7 +7,7 @@ class SimpleTorqueController:
         self.length = 41.86
         self.g = 9.8
         self.A = [[0, 1], [self.g / self.length, 0]]
-        self.B = [[0], [1 / (self.m * self.length ^ 2)]]
+        self.B = [[0], [1 / (self.m * (self.length ** 2))]]
         self.C = [[1, 0], [0, 1]]
         self.D = [0, 0]
         self.poles = [-10 - 20]
