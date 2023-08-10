0b91231d545c2a0ebdbc763d8533f30aa838c2cc
NhiNguyencmt8 <61450663+NhiNguyencmt8@users.noreply.github.com>
2023-08-10 15:04:38 -0400

    testing

--- RealRobot/main.py
+++ RealRobot/main.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
 
     while time.time() - start_time < 4:  # seconds
 
-        torque = knee_control.torque_linear_controller(10,
+        torque = knee_control.torque_linear_controller(0.1745,
                                                        right_knee_pitch_joint.get_velocity(),
                                                        desiredPos,
                                                        desiredVel)
